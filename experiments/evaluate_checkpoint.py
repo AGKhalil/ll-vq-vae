6ac0b369df3f00554c85ed30c061e69fe6c6886b
AGKhalil <elghazalykhalil@gmail.com>
2023-11-15 13:55:01 +0000

    made sure all recons are facing the same direction

--- experiments/evaluate_checkpoint.py
+++ experiments/evaluate_checkpoint.py
@@ -5,6 +5,7 @@ import hydra
 from hydra.core.hydra_config import HydraConfig
 from omegaconf import DictConfig, OmegaConf
 import torch
+import torchvision
 from utils import count_uniques, reconstruct_images
 from model import Model
 from data_modules import (
@@ -70,39 +71,49 @@ def main(cfg: DictConfig) -> None:
     model = model.to("cuda" if torch.cuda.is_available() else "cpu")
     model.eval()
 
-    grid = reconstruct_images(dataloader=data.test_dataloader, model=model)
-    plt.imshow(grid.cpu().permute(1, 2, 0))
-    plt.savefig(
-        os.path.join(
-            "reconstructions",
-            dataset_choice,
-            f"{quantizer_choice}.png",
-        ),
-        dpi=600,
-    )
-
     if "dense" in quantizer_choice:
         print(
             "The dense lattice assigns a quantization point per embedding"
             " vector. So the codebook is too large to count."
         )
-        return
+    else:
+        codebook_usage = count_uniques(
+            model=model,
+            dataloaders=[
+                data.train_dataloader,
+                data.val_dataloader,
+            ],
+        )
 
-    codebook_usage = count_uniques(
-        model=model,
-        dataloaders=[
-            data.train_dataloader,
-            data.val_dataloader,
-        ],
-    )
+        json_path = os.path.join(
+            "reconstructions",
+            dataset_choice,
+            f"{quantizer_choice}.json",
+        )
+        with open(json_path, "w") as json_file:
+            json.dump(codebook_usage, json_file)
 
-    json_path = os.path.join(
-        "reconstructions",
-        dataset_choice,
-        f"{quantizer_choice}.json",
+    for idx, transform in enumerate(data.transform.transforms):
+        if (
+            transform.__class__.__name__
+            == torchvision.transforms.RandomHorizontalFlip().__class__.__name__
+        ):
+            data.transform.transforms.pop(idx)
+    grid = reconstruct_images(dataloader=data.val_dataloader, model=model)
+    fig = plt.figure(frameon=False)
+    ax = plt.Axes(fig, [0.0, 0.0, 1.0, 1.0])
+    ax.set_axis_off()
+    fig.add_axes(ax)
+    ax.imshow(grid.cpu().permute(1, 2, 0))
+    fig.savefig(
+        os.path.join(
+            "reconstructions",
+            dataset_choice,
+            f"{quantizer_choice}.png",
+        ),
+        bbox_inches="tight",
+        dpi=600,
     )
-    with open(json_path, "w") as json_file:
-        json.dump(codebook_usage, json_file)
 
 
 if __name__ == "__main__":
